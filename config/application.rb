4cfad7c51bfe2a51a14328df9d420badb6545c1b
Elena Graivoronskaya <helenraven@mail.ru>
2020-12-01 15:55:43 +0700

    Lesson1 ready

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,17 @@ module TestGuru
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.time_zone = 'Krasnoyarsk'
+
+    # Автозагрузка всех языковых файлов
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+
+    # Дефолтовый язык
+    config.i18n.default_locale = :ru
+
+    # Возврат к английскому при ошибках
+    config.i18n.fallbacks = [:en]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
