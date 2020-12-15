0144865ddfbf16d1889099fab5c3c7bb4bc8724b
Elena Graivoronskaya <helenraven@mail.ru>
2020-12-15 16:33:34 +0700

    Tests/Questions controllers - fixed

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,14 +1,12 @@
 class QuestionsController < ApplicationController
 
   before_action :find_question, only: %i[show destroy]
-  before_action :all_questions, only: %i[index]
-  before_action :find_test, only: %i[create]
-
+  before_action :find_test, only: %i[index create]
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
     result = "#{params.inspect}\n\n"
-    @questions.each do |question|
+    @test.questions.each do |question|
       result += "id: #{question.id}, test_id: #{question.test_id}, body: #{question.body}\n"
     end
     render plain: result
@@ -22,19 +20,20 @@ class QuestionsController < ApplicationController
   def create
     @question = @test.questions.new(question_params)
     if @question.save
-      render plain: @question.inspect
+      redirect_to test_questions_path(@test)
     else
       render :new
     end
   end
 
   def show
-    render inline: '<%= @question.body%>'
+    @question = Question.find(params[:id])
+    render plain: @question.body
   end
 
   def destroy
     @question.destroy
-    redirect_to questions_path
+    redirect_to test_questions_path(@test)
   end
 
   private
@@ -47,12 +46,8 @@ class QuestionsController < ApplicationController
     @question = Question.find(params[:id])
   end
 
-  def all_questions
-    @questions = params[:test_id].nil? ? Question.all : Test.find(params[:test_id].to_i).questions
-  end
-
   def find_test
-    @test = Test.find(params[:test_id].to_i)
+    @test = Test.find(params[:test_id])
   end
 
   def rescue_with_question_not_found
--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,14 +1,13 @@
 class TestsController < ApplicationController
 
   before_action :find_test, only: %i[show destroy]
-  before_action :set_category_user, only: %i[create]
-  before_action :all_tests, only: %i[index]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
 
   def index
     result = "Params: #{params.inspect}\n\n"
-    @tests.each do |test|
+    tests = Test.all
+    tests.each do |test|
       result += "id: #{test.id}, title: #{test.title}\n"
     end
     render plain: result
@@ -19,15 +18,7 @@ class TestsController < ApplicationController
   end
 
   def create
-    result = "#{params.inspect}\n"
-    @test = @category.tests.new(test_params)
-    @test.author = @user
-    result += "#{@test.inspect}"
-    if @test.save
-      render plain: result
-    else
-      render :new
-    end
+    render plain: params.inspect
   end
 
   def show
@@ -47,15 +38,6 @@ class TestsController < ApplicationController
 
   def find_test
     @test = Test.find(params[:id])
-  end
-
-  def all_tests
-    @tests = Test.all
-  end
-
-  def set_category_user
-    @category = Category.find(params[:category_id].to_i)
-    @user = User.first
   end
 
   def rescue_with_test_not_found
