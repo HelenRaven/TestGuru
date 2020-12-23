883142e18629901f4d93ef56826c50574af4a4ec
HelenRaven <helenraven@mail.ru>
2020-12-23 23:15:19 +0700

    Authentication with Devise

---
+++ app/controllers/admin/tests_controller.rb
@@ -0,0 +1,62 @@
+class Admin::TestsController < Admin::BaseController
+
+  before_action :find_test, only: %i[show edit update destroy start]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
+
+  def index
+    @tests = Test.all
+  end
+
+  def new
+    @test = Test.new
+  end
+
+  def show; end
+
+  def create
+    @test = Test.new(test_params)
+    @test.author = current_user
+
+    if @test.save
+      redirect_to [:admin, @test]
+    else
+      render :new
+    end
+
+  end
+
+  def edit; end
+
+  def update
+    if @test.update(test_params)
+      redirect_to [:admin, @test]
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @test.destroy
+    redirect_to admin_tests_path
+  end
+
+  def start
+    current_user.tests.push(@test)
+    redirect_to current_user.result(@test)
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:id])
+  end
+
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id)
+  end
+
+  def rescue_with_test_not_found
+    render plain: "Test was not found"
+  end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,29 +1,26 @@
 class ApplicationController < ActionController::Base
 
-  #protect_from_forgery with: :exception
-
-
-  helper_method :current_user,
-                :logged_in?
-
+  protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
-  def authenticate_user!
-    unless current_user
-      redirect_to login_path, alert: 'Verify your email and password please'
-      cookies[:user_requested_path] = request.original_fullpath
+  def after_sign_in_path_for(resource)
+
+    if current_user.sign_in_count == 0
+      flash[:notice] = "Welcome, #{current_user.first_name} #{current_user.last_name}!"
     end
 
-    cookies[:email] = current_user&.email
+    if current_user.is_a?(Admin)
+      admin_tests_path
+    else
+      tests_path
+    end
   end
 
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
   end
 
-  def logged_in?
-    current_user.present?
-  end
 end
