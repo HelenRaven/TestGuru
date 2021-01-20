882d60daabb7e6eacd8594c57c90260c5f4fd7d8
HelenRaven <helenraven@mail.ru>
2021-01-20 18:17:39 +0700

    migrations prepare

--- db/migrate/20201218112316_add_fields_to_result.rb
+++ db/migrate/20201218112316_add_fields_to_result.rb
@@ -3,7 +3,7 @@ class AddFieldsToResult < ActiveRecord::Migration[6.0]
   def change
     change_table :results do |t|
       t.rename :progress, :correct_questions
-      t.remove :result, :string, default: '0'
+      t.remove :result
       t.belongs_to :question, foreign_key: true
     end
   end
