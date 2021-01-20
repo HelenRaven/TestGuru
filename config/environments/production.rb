83f5ec76b5b651eeb76fbf45ea70577d29e2a8f6
HelenRaven <helenraven@mail.ru>
2021-01-20 18:51:09 +0700

    Mailer instructions

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,4 +111,16 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   config.secret_key_base = ENV['SECRET_KEY_BASE']
+
+  config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'guru-of-tests-app.herokuapp.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:                'smtp.gmail.com',
+    port:                   587,
+    user_name:              ENV['SMTP_USERNAME'],
+    password:               ENV['SMTP_PASSWORD'],
+    authentication:         'plain',
+    enable_starttls_auto:  true
+  }
 end
