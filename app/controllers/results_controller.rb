1ce811b1d97852b2ec9515e1cdc4d0c78b56c0de
HelenRaven <helenraven@mail.ru>
2021-02-02 14:47:47 +0700

    create HandlingBadgesService

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -5,12 +5,12 @@ class ResultsController < ApplicationController
   def show; end
 
   def finish
-    #если это поместить в update, получится вложенный if
     if @result.test_passed?
       @result.passed = true
       @result.save
 
-      hand_out_badges
+      service = HandlingBadgesService.new(current_user)
+      service.hand_out_badges
     end
   end
 
@@ -43,51 +43,10 @@ class ResultsController < ApplicationController
     redirect_to @result, flash_options
   end
 
-  def hand_out_badges
-      Badge.all.each do |badge|
-        public_send(badge.rule.to_sym, param: badge.parameter, badge: badge)
-    end
-  end
-
-  def all_in_category(param:, badge:)
-    test_ary = Test.not_empty_tests.with_category(param).pluck(:id).sort
-    check_rule(param, badge, test_ary)
-  end
-
-  def all_at_level(param:, badge:)
-    test_ary = Test.not_empty_tests.with_level(param).pluck(:id).sort
-    check_rule(param, badge, test_ary)
-  end
-
-  def for_first_time(param:, badge:)
-    results = current_user.results.test_with_title(param)
-    if results.count == 1
-      current_user.badges.push(badge)
-      results.last.badges.push(badge)
-    end
-  end
-
   private
 
   def set_result
     @result = Result.find(params[:id])
-  end
-
-  def unbaged_results
-    badged_results_ids = current_user.results.passed.with_badges.pluck(:id)
-    current_user.results.passed.except_ids(badged_results_ids)
-  end
-
-  def check_rule(param, badge, test_ary)
-    results = unbaged_results
-    result_ary = results.pluck(:test_id).uniq.sort
-
-    if result_ary == test_ary
-      current_user.badges.push(badge)
-      results.tests_results(test_ary).each do |result|
-        result.badges.push(badge)
-      end
-    end
   end
 
 end
